c6dd53a7cc679878f5efb47fcb20899658f6ed04
Evgeniievg <evgeniy.litvinv01@gmail.com>
2023-11-09 18:07:48 +0400

    popups

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ const closeButton = document.querySelector('.header__menu-close');
 const mobileMenu = document.querySelector('.header__menu-container');
 const openButton = document.querySelector('.header__menu-image');
 const mobileMenuBackground = document.querySelector('.header__mobile-menu');
+const menuElement = document.querySelectorAll('.header__element');
+const firstProjectLink = document.querySelector('.projects__subtitle_type_first');
+const secondProjectLink = document.querySelector('.projects__subtitle_type_second');
+const firstProject = document.querySelector('.first-project');
+const secondProject = document.querySelector('.second-project');
+const closePopupButton = document.querySelector('.first-project__close')
+const closeSecondPopupButton = document.querySelector('.second-project__close')
+
+
+firstProjectLink.addEventListener('click', () => {
+  firstProject.classList.toggle('open')
+})
+
+secondProjectLink.addEventListener('click', () => {
+  secondProject.classList.toggle('open')
+})
+
+closePopupButton.addEventListener('click', () => {
+  firstProject.classList.remove('open')
+})
+
+closeSecondPopupButton.addEventListener('click', () => {
+  secondProject.classList.remove('open')
+})
+
+menuElement.forEach((e) => e.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+  mobileMenuBackground.classList.remove('open')
+}))
 
 closeButton.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
@@ -12,28 +41,3 @@ openButton.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
   mobileMenuBackground.classList.toggle('open')
 })
-function isElementInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-function handleScrollAnimation() {
-  const elements = document.querySelectorAll('.fade-in');
-
-  elements.forEach((element) => {
-    if (isElementInViewport(element)) {
-      element.classList.add('visible');
-    }
-  });
-}
-
-// Add a scroll event listener to trigger the animation
-window.addEventListener('scroll', handleScrollAnimation);
-
-// Initial check for elements in the viewport
-handleScrollAnimation();
